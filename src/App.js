dc972875fda085398b2c3441cbc8b3d1629c4d8c
João Victor <joaovictorrdesouza1@gmail.com>
2019-10-08 23:33:08 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333'
+})
 
 function App() {
+  const [newPostContent, setNewPostContent] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      const { data: posts} = await api.get('/posts')
+
+      setPosts(posts)
+    }
+
+    loadPosts()
+  }, [])
+
+  async function handleSave(e){
+    e.preventDefault()
+
+    //desestruturando o atributo data da resposta e atribuindo esse a uma variavel chamada post
+    const { data: post} = await api.post('/posts', {
+      content: newPostContent
+    })
+
+    setPosts([...posts, post])
+    setNewPostContent('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSave}>
+        <textarea 
+          onChange={e => setNewPostContent(e.target.value)}
+          value={newPostContent}
+        />
+        <button type="submit">Postar</button>
+      </form>
+
+      <ul>
+        {posts.map(post => (
+          <li key={post.id} >{post.content}</li>
+        ))}
+      </ul>
     </div>
   );
 }
